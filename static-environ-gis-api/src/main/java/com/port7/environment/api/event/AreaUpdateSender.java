bac3acc101219622ebfa9f0a19fecd1822472373
Stephen Badger <stephen.badger@gmail.com>
2013-03-23 14:50:53 +0100

    Adds a bean that sends area updates to subscribers.

---
+++ static-environ-gis-api/src/main/java/com/port7/environment/api/event/AreaUpdateSender.java
@@ -0,0 +1,46 @@
+package com.port7.environment.api.event;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.enterprise.event.Observes;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+
+import com.port7.environment.model.event.AreaDataChangeEvent;
+
+@Singleton
+@LocalBean
+public class AreaUpdateSender {
+	@Resource(mappedName = "java:/JmsXA")
+	private static ConnectionFactory connectionFactory;
+	
+	@Resource(mappedName = "static-environ-gis/area-updates")
+	private Destination topic;
+	
+	private Connection connection;
+	
+	@PostConstruct
+	public void initialise() throws JMSException {
+		connection = connectionFactory.createConnection();
+	}
+	
+	@PreDestroy
+	public void tearDown() throws JMSException {
+		connection.close();
+	}
+	
+	public void receiveEvent(@Observes AreaDataChangeEvent event) throws JMSException {
+		Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+		MessageProducer producer = session.createProducer(topic);
+		ObjectMessage message = session.createObjectMessage(event);
+		producer.send(message);
+	}
+}
