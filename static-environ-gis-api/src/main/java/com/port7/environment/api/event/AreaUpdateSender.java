76942de2a802b6d737adf164038cf46bdf11784e
Stephen Badger <stephen.badger@gmail.com>
2013-03-23 16:07:12 +0100

    Corrects topic sending in Area Updates.

--- static-environ-gis-api/src/main/java/com/port7/environment/api/event/AreaUpdateSender.java
+++ static-environ-gis-api/src/main/java/com/port7/environment/api/event/AreaUpdateSender.java
@@ -22,23 +22,26 @@ public class AreaUpdateSender {
 	@Resource(mappedName = "java:/JmsXA")
 	private static ConnectionFactory connectionFactory;
 	
-	@Resource(mappedName = "static-environ-gis/area-updates")
+	@Resource(mappedName = "java:/static-environ-gis/area-updates")
 	private Destination topic;
 	
 	private Connection connection;
 	
+	private Session session;
+	
 	@PostConstruct
 	public void initialise() throws JMSException {
-		connection = connectionFactory.createConnection();
+		connection 	= connectionFactory.createConnection();
+		session 	= connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
 	}
 	
 	@PreDestroy
 	public void tearDown() throws JMSException {
+		session.close();
 		connection.close();
 	}
 	
 	public void receiveEvent(@Observes AreaDataChangeEvent event) throws JMSException {
-		Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
 		MessageProducer producer = session.createProducer(topic);
 		ObjectMessage message = session.createObjectMessage(event);
 		producer.send(message);
